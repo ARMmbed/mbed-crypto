3681fc9a76bf4da7e90d2acf86c95b5e812bb3bc
Michael Thomas <michael.thomas@renesas.com>
2020-03-13 20:01:51 +0000

    Updated to allow psa_key_open to handle vendor defined keys

--- library/psa_crypto_slot_management.c
+++ library/psa_crypto_slot_management.c
@@ -147,6 +147,14 @@ static psa_status_t psa_load_persistent_key_into_slot( psa_key_slot_t *slot )
     }
     else
 #endif /* MBEDTLS_PSA_CRYPTO_SE_C */
+#if defined (MBEDTLS_PSA_CRYPTO_ACCEL_DRV_C)
+    if (PSA_KEY_LIFETIME_IS_VENDOR_DEFINED(slot->attr.lifetime))
+    {
+        status = psa_import_key_into_slot_vendor( slot, key_data, key_data_length);
+            goto exit;
+    }
+    else
+#endif /* MBEDTLS_PSA_CRYPTO_ACCEL_DRV_C */
     {
         status = psa_import_key_into_slot( slot, key_data, key_data_length );
     }
